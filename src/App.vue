204b649c9027533df1bf115ef906c1adc2cb4a8d
faizuddin53 <faizuddinansari53@gmail.com>
2025-12-31 20:37:54 +0530

    header added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import Navbar from './components/Navbar.vue';
-
+import Header from './components/Header.vue';
 </script>
 
 <template>
-       <Navbar/>
+  <!-- Header -->
+   <Header/>
+
 </template>
 
 <style scoped>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="headerContainer">
+    <div>
+      <h1>Aligarh Shop</h1>
+    </div>
+    <div>
+      <input class="searchBar" placeholder="Search Product" />
+    </div>
+    <div class="headerButton">
+      <button style="margin-right: 10px">Cart</button>
+      <button>Login</button>
+    </div>
+  </div>
+</template>
+<script></script>
+<style scoped>
+.headerContainer {
+  display: flex;
+  justify-content: space-between;
+  padding: 5px;
+  background-color: #e1d0d0;
+  margin: 8px;
+}
+
+.headerContainer .headerButton button {
+  border-radius: 5px;
+  font-weight: 600;
+  width: 50px;
+  height: 30px;
+  cursor: pointer;
+}
+
+.headerContainer .searchBar {
+  padding: 5px;
+  border-radius: 5px;
+  height: 35px;
+  width: 400px;
+}
+.headerContainer .headerButton button:hover {
+  background-color: rgb(148, 99, 99);
+}
+</style>
